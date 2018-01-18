8cd463baeed95e274a59e501674309f8e2fca7b7
RamyNasrEldin <34554820+RamyNasrEldin@users.noreply.github.com>
2018-01-18 16:03:35 +0200

    Update config.ts

--- VehicleMonitoring.WebSPA/src/app/config/config.ts
+++ VehicleMonitoring.WebSPA/src/app/config/config.ts
@@ -1,9 +1,9 @@
 export let Config = {
   Urls: {
-    customerUrl: 'http://localhost:3000/api/Customers',
-    customerVehicleListUrl: 'http://localhost:3000/api/MonitoringBoard/GetCustomersVehicles',
-    vehiclesByCutomerIdUrl: 'http://localhost:3000/api/MonitoringBoard/GetCustomerVehiclesByCustomerId',
-    vehiclesByStatusUrl: 'http://localhost:3000/api/MonitoringBoard/GetCustomersVehiclesByStatus',
+    customerUrl: 'http://localhost:5000/api/Customers',
+    customerVehicleListUrl: 'http://localhost:5000/api/MonitoringBoard/GetCustomersVehicles',
+    vehiclesByCutomerIdUrl: 'http://localhost:5000/api/MonitoringBoard/GetCustomerVehiclesByCustomerId',
+    vehiclesByStatusUrl: 'http://localhost:5000/api/MonitoringBoard/GetCustomersVehiclesByStatus',
     signalrUrl:'http://localhost:4000/vehicleMonitoring'
   }
 };
