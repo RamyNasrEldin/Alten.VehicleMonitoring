2392820aaaaa7f0998617fd759508d7ad94d2fc1
RamyNasrEldin <ramy.mne@gmail.com>
2018-01-18 12:52:35 +0200

    edite some configurations

--- VehicleMonitoring.VehicleAvatarService.Simulator/Program.cs
+++ VehicleMonitoring.VehicleAvatarService.Simulator/Program.cs
@@ -6,40 +6,27 @@ namespace VehicleMonitoring.VehicleAvatarService.Simulator
 {
     class Program
     {
-       
+
         static void Main(string[] args)
         {
             try
             {
                 var serviceCollection = new ServiceCollection();
-
-                //new Startup().ConfigureServices(serviceCollection);
                 ContainerConfig.ConfigureServices(serviceCollection);
                 var serviceProvider = serviceCollection.BuildServiceProvider();
-
                 var _logger = serviceProvider.GetRequiredService<ILogger<Program>>();
-                Console.WriteLine("Hello, Welcome to Alten Simulator,\nDo you want to start sending random status values for the fleet vehicles?");
-                var result = Console.ReadKey();
-                Console.WriteLine("\n");
-                if (result.Key == ConsoleKey.Y)
+                Console.WriteLine("Hello, Welcome to Alten Simulator.\n");
+                try
                 {
-                    try
-                    {
-                        var application = serviceProvider.GetService<App>();
-                        application.Run();
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogCritical(ex.Message);
-                    }
+                    var application = serviceProvider.GetService<App>();
+                    application.Run();
                 }
-                else
+                catch (Exception ex)
                 {
-                    Console.WriteLine("\n******          Thank you for using our simulator         ****** \nPlease press any key to exit");
-                    Console.ReadKey();
+                    _logger.LogCritical(ex.Message);
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
--- VehicleMonitoring.WebSPA/src/app/config/config.ts
+++ VehicleMonitoring.WebSPA/src/app/config/config.ts
@@ -1,9 +1,9 @@
 export let Config = {
   Urls: {
-    customerUrl: 'http://localhost:27819/api/Customers',
-    customerVehicleListUrl: 'http://localhost:27819/api/MonitoringBoard/GetCustomersVehicles',
-    vehiclesByCutomerIdUrl: 'http://localhost:27819/api/MonitoringBoard/GetCustomerVehiclesByCustomerId',
-    vehiclesByStatusUrl: 'http://localhost:27819/api/MonitoringBoard/GetCustomersVehiclesByStatus',
-    signalrUrl:'http://localhost:5688/vehicleMonitoring'
+    customerUrl: 'http://localhost:3000/api/Customers',
+    customerVehicleListUrl: 'http://localhost:3000/api/MonitoringBoard/GetCustomersVehicles',
+    vehiclesByCutomerIdUrl: 'http://localhost:3000/api/MonitoringBoard/GetCustomerVehiclesByCustomerId',
+    vehiclesByStatusUrl: 'http://localhost:3000/api/MonitoringBoard/GetCustomersVehiclesByStatus',
+    signalrUrl:'http://localhost:4000/vehicleMonitoring'
   }
 };
